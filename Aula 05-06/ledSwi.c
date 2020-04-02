545e3d629fadf6d12f4092c9708fedb90e8b9d0c
cvillela <csvillela1@gmail.com>
2020-04-02 15:30:58 -0300

    .c and .h description included

---
+++ Aula 05-06/ledSwi.c
@@ -0,0 +1,10 @@
+/* ***************************************************************** */
+/* File name:        ledSwi.c                                        */
+/* File description: This file implements the functions needed to    */
+/*                   initialize, switch in between and operate RGB   */
+/*                   leds and buttons                                */
+/*                                                                   */
+/* Author name:      Caio Villela/ Hebert Wandick                    */
+/* Creation date:    02apr2020                                       */
+/* Revision date:    02apr2020                                       */
+/* ***************************************************************** */
