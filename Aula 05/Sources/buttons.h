c5a8566684657a1e7b319f12acaf0d66225857f4
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-04-02 15:49:03 -0300

    Aula 05 criada no KDS

---
+++ Aula 05/Sources/buttons.h
@@ -0,0 +1,21 @@
+/*
+ * buttons.h
+ *
+ *  Created on: 05/03/2020
+ *      Author: aluno
+ */
+
+#ifndef SOURCES_BUTTONS_H_
+#define SOURCES_BUTTONS_H_
+
+
+void buttons_init(void);
+
+
+int button_read(void);
+
+
+
+
+
+#endif /* SOURCES_BUTTONS_H_ */
