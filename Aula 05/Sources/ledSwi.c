05624d03010343eacfc3a2ccd77862697a8b96a2
cvillela <csvillela1@gmail.com>
2020-04-03 13:49:48 -0300

    deixando mais elegante

--- Aula 05/Sources/ledSwi.c
+++ Aula 05/Sources/ledSwi.c
@@ -18,8 +18,6 @@
 #include "fsl_port_hal.h"
 #include "fsl_gpio_hal.h"
 
-#define
-
 void initLedButton(int *iPinsLed, int iTamLed,int *iPinsButton, int iTamButton){
     int iI = 0; /*integer iterator*/
     uint32_t uiLedPins; /*selected led pins are 1*/
@@ -32,39 +30,33 @@ void initLedButton(int *iPinsLed, int iTamLed,int *iPinsButton, int iTamButton){
     if(iTamLed != 0){
         for(iI = 0; iI < iTamLed; iI ++;){
             if(1 == iPinsLed[i]){
-                PORTA_PCR1 |= 0x100
-                GPIOA_PDDR |= LED1_PIN
+                PORTA_PCR1 |= 0x100;
+                GPIOA_PDDR |= LED1_PIN;
             }else if(2 == iPinsLed[i]){
-                PORTA_PCR2 |= 0x100
-                GPIOA_PDDR |= LED2_PIN
+                PORTA_PCR2 |= 0x100;
+                GPIOA_PDDR |= LED2_PIN;
             }else if(3 == iPinsLed[i]){
-                PORTA_PCR4 |= 0x100
-                GPIOA_PDDR |= LED3_PIN
+                PORTA_PCR4 |= 0x100;
+                GPIOA_PDDR |= LED3_PIN;
             }else if(4 == iPinsLed[i]){
-                PORTA_PCR5 |= 0x100
-                GPIOA_PDDR |= LED4_PIN
+                PORTA_PCR5 |= 0x100;
+                GPIOA_PDDR |= LED4_PIN;
             }
         }
-
-
     }
-
 
     /*initialize selected buttons as GPIO and set them as digital input*/
     if(iTamButton != 0){
+        GPIOA_PDDR |= (uint32_t) 0u;
         for(iI = 0; iI < iTamButton; iI ++;){
             if(1 == iPinsButton[i]){
-                PORTA_PCR1 |= 0x100
-                GPIOA_PDDR |= (uint32_t) 0u
+                PORTA_PCR1 |= 0x100;
             }else if(2 == iPinsButton[i]){
-                PORTA_PCR2 |= 0x100
-                GPIOA_PDDR |= (uint32_t) 0u
+                PORTA_PCR2 |= 0x100;
             }else if(3 == iPinsButton[i]){
-                PORTA_PCR4 |= 0x100
-                GPIOA_PDDR |= (uint32_t) 0u
+                PORTA_PCR4 |= 0x100;
             }else if(4 == iPinsButton[i]){
-                PORTA_PCR5 |= 0x100
-                GPIOA_PDDR |= (uint32_t) 0u
+                PORTA_PCR5 |= 0x100;
             }
         }
     }
