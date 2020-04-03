ed0f24cf2a6c7b2b828949a2f1e5e35f8d0871f2
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-04-02 22:26:45 -0300

    funcion implemented in ledSwi.c, less init

--- Aula 05/Sources/ledSwi.c
+++ Aula 05/Sources/ledSwi.c
@@ -19,3 +19,83 @@
 #include "fsl_gpio_hal.h"
 
 #define
+
+void toggleLed(int iPin)
+{
+    switch (iPin)
+    {
+    case 1:
+        GPIOA_PTOR |= 0x00;
+        break;
+    case 2:
+        GPIOA_PTOR |= 0x02;
+        break;
+    case 3:
+        GPIOA_PTOR |= 0x04;
+        break;
+    case 4:
+        GPIOA_PTOR |= 0x08;
+        break;
+    };
+}
+
+void writeLed(int iPin, int iNivel)
+{
+    if (iNivel)
+    {
+        switch (iPin)
+        {
+        case 1:
+            GPIOA_PSOR|=0x00;
+            break;
+        case 2:
+            GPIOA_PSOR|=0x02;
+            break;
+        case 3:
+            GPIOA_PSOR|=0x04;
+            break;
+        case 4:
+            GPIOA_PSOR|=0x08;
+            break;
+        };
+    }
+    else
+    {
+        case 1:
+            GPIOA_PCOR|=0x00;
+            break;
+        case 2:
+            GPIOA_PCOR|=0x02;
+            break;
+        case 3:
+            GPIOA_PCOR|=0x04;
+            break;
+        case 4:
+            GPIOA_PCOR|=0x08;
+            break;
+        };
+    }
+}
+
+void ligaLed(int iPin){
+    writeLed(iPin,0);
+};
+
+void desligaLed(int iPin){
+    writeLed(iPin,1);
+}
+
+int readButton(int iPin){
+    case 1:
+        GPIOA_PDIR |= 0x00;
+        break;
+    case 2:
+        GPIOA_PDIR |= 0x02;
+        break;
+    case 3:
+        GPIOA_PDIR |= 0x04;
+        break;
+    case 4:
+        GPIOA_PDIR |= 0x08;
+        break;
+}
