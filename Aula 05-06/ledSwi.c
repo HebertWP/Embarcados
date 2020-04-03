e30eaddd0ff512d6eb6f5ca52d3ea418dadd8b03
cvillela <csvillela1@gmail.com>
2020-04-03 13:08:15 -0300

    board.h pinnage fixed, initLedButton finished

--- Aula 05-06/ledSwi.c
+++ Aula 05-06/ledSwi.c
@@ -18,4 +18,73 @@
 #include "fsl_port_hal.h"
 #include "fsl_gpio_hal.h"
 
+void initLedButton(int *iPinsLed, int iTamLed,int *iPinsButton, int iTamButton){
+    int iI = 0; /*integer iterator*/
+    uint32_t uiLedPins; /*selected led pins are 1*/
+    uint32_t uiButtonPins; /*selected button pins are 1*/
+
+    /*un-gateport A clock*/
+    SIM_SCGC5|=0x200;
+
+
+
+    /*initialize selected Leds as GPIO and set them as digital output*/
+    if(iTamLed != 0){
+        for(iI = 0; iI < iTamLed; iI ++;){
+            if(1 == iPinsLed[i]){
+                PORTA_PCR1 |= 0x100
+                GPIOA_PDDR |= LED1_PIN
+            }else if(2 == iPinsLed[i]){
+                PORTA_PCR2 |= 0x100
+                GPIOA_PDDR |= LED2_PIN
+            }else if(3 == iPinsLed[i]){
+                PORTA_PCR4 |= 0x100
+                GPIOA_PDDR |= LED3_PIN
+            }else if(4 == iPinsLed[i]){
+                PORTA_PCR5 |= 0x100
+                GPIOA_PDDR |= LED4_PIN
+            }
+        }
+
+
+    }
+
+
+    /*initialize selected buttons as GPIO and set them as digital input*/
+    if(iTamButton != 0){
+        for(iI = 0; iI < iTamButton; iI ++;){
+            if(1 == iPinsButton[i]){
+                PORTA_PCR1 |= 0x100
+                GPIOA_PDDR |= (uint32_t) 0u
+            }else if(2 == iPinsButton[i]){
+                PORTA_PCR2 |= 0x100
+                GPIOA_PDDR |= (uint32_t) 0u
+            }else if(3 == iPinsButton[i]){
+                PORTA_PCR4 |= 0x100
+                GPIOA_PDDR |= (uint32_t) 0u
+            }else if(4 == iPinsButton[i]){
+                PORTA_PCR5 |= 0x100
+                GPIOA_PDDR |= (uint32_t) 0u
+            }
+        }
+    }
+
+
+
+}
+
+
+int readButton(int iPin);
+
+void ligaLed(int iPin);
+
+void desligaLed(int iPin);
+
+void toggleLed(int iPin);
+
+void writeLed(int iPin, int iNivel);
+
+
+
+
 #define
--- Aula 05/Sources/board.h
+++ Aula 05/Sources/board.h
@@ -29,34 +29,34 @@
 
 #define BUTTON1_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define BUTTON1_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define BUTTON1_PIN	         (uint32_t) 1u
+#define BUTTON1_PIN	         (uint32_t) 2u
 
 #define BUTTON2_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define BUTTON2_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define BUTTON2_PIN	         (uint32_t) 2u
+#define BUTTON2_PIN	         (uint32_t) 3u
 
 #define BUTTON3_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define BUTTON3_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define BUTTON3_PIN	         (uint32_t) 4u
+#define BUTTON3_PIN	         (uint32_t) 5u
 
 #define BUTTON4_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define BUTTON4_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define BUTTON4_PIN	         (uint32_t) 5u
+#define BUTTON4_PIN	         (uint32_t) 6u
 
 #define LED1_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define LED1_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define LED1_PIN	         (uint32_t) 1u
+#define LED1_PIN	         (uint32_t) 2u
 
 #define LED2_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define LED2_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define LED2_PIN	         (uint32_t) 2u
+#define LED2_PIN	         (uint32_t) 3u
 
 #define LED3_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define LED3_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define LED3_PIN	         (uint32_t) 4u
+#define LED3_PIN	         (uint32_t) 5u
 
 #define LED4_PORT_BASE_PNT   PORTA /* peripheral port base pointer */
 #define LED4_GPIO_BASE_PNT   PTA   /* peripheral gpio base pointer */
-#define LED4_PIN	         (uint32_t) 5u
+#define LED4_PIN	         (uint32_t) 6u
 
 #endif /* SOURCES_BOARD_H_ */
