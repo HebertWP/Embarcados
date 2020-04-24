5aba0fd45feb045668141e44cdebc4d162b2f07f
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-04-24 14:26:06 -0300

    init cooler and header prog with main.c done

---
+++ Aula 11-12/Sources/aquecedorECooler.c
@@ -0,0 +1,53 @@
+/* ************************************************ */
+/* Method name:        PWM_init	                    */
+/* Method description: Initialize PWM				*/
+/* Input params:       n/a                          */
+/* Output params:      n/a                          */
+/* ************************************************ */
+void PWM_init(void){
+
+}
+
+/* ************************************************ */
+/* Method name:        coolerfan_init               */
+/* Method description: Initialize coolerfan			*/
+/* Input params:       n/a                          */
+/* Output params:      n/a                          */
+/* ************************************************ */
+void coolerfan_init(void){
+
+}
+
+/* ************************************************ */
+/* Method name:        heater_init		            */
+/* Method description: Initialize heater			*/
+/* Input params:       n/a                          */
+/* Output params:      n/a                          */
+/* ************************************************ */
+void heater_init(void){
+
+}
+
+/* ************************************************ */
+/* Method name:        coolerfan_PWMDuty            */
+/* Method description: set the coolerfan speed		*/
+/* Input params:       fCoolerDuty how many 		*/
+/* 					   percentage time the cooler 	*/
+/* 					   will be in high				*/
+/* Output params:      n/a                          */
+/* ************************************************ */
+void coolerfan_PWMDuty(float fCoolerDuty){
+
+}
+
+/* ************************************************ */
+/* Method name:        heater_PWMDuty	            */
+/* Method description: set the heater power			*/
+/* Input params:       fHeaterDuty how many 		*/
+/* 					   percentage time the cooler 	*/
+/* 					   will be in high				*/
+/* Output params:      n/a                          */
+/* ************************************************ */
+void heater_PWMDuty(float fHeaterDuty){
+
+}
