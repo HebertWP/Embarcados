7e5c9970a2897c7cbbe6945fafa28b63aa19fdcf
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-04-25 08:13:27 -0300

    put more config params, at board.h

--- Aula 11-12/Sources/aquecedorECooler.c
+++ Aula 11-12/Sources/aquecedorECooler.c
@@ -6,34 +6,30 @@
 /* ************************************************ */
 
 #include "board.h"
-
+#include "aquecedorECooler.h"
 #include "fsl_clock_manager.h"
 
 void PWM_init(void){
-	/*un-gateport C clock*/
-	SIM_SCGC5 |= 0x0800;
+	/*un-gateport A clock*/
+	SET_BITS(SIM_SCGC5, CGC_CLOCK_ENABLED, 1, PORT_A_TO_SHIFT);
 
 	/*un-gateport C clock*/
-	TURN_ON_BITS(SIM_SCGC6, 0b01U, 25);
+	SET_BITS(SIM_SCGC6, CGC_CLOCK_ENABLED, 1, 25);
 
 	/*Prescale Factor Selection, 110 Divide by 64*/
-	TURN_OFF_BITS(TPM1_SC,0b111U, 0);
-	TURN_ON_BITS(TPM1_SC, 0b110U, 0);
+	SET_BITS(TPM1_SC, PRESCALER, 3, 0);
 
 	/*TPM clock source select, 11 MCGIRCLK clock (32KHz)*/
-	TURN_ON_BITS(SIM_SOPT2, 0b011U, 24);
+	SET_BITS(SIM_SOPT2, TPM_CLOCK, 2, 24);
 
 	/*Clock Mode Selection *,01 LPTPM counter increments on every LPTPM counter clock */
-	TURN_OFF_BITS(TPM1_SC,0b11U, 3);
-	TURN_ON_BITS(TPM1_SC, 0b01U, 3);
+	SET_BITS(TPM1_SC, TPM_CMOD, 2, 3);
+
 	/*Center-aligned PWM Select,0 LPTPM counter operates in up counting mode.*/
-	TURN_OFF_BITS(TPM1_SC,0b01U, 5);
+	SET_BITS(TPM1_SC, TPM_CPWMS, 1, 5);
 
-	TURN_OFF_BITS(TPM1_MOD,0xFFFFU, 0);
-	TURN_ON_BITS(TPM1_MOD, 49U, 0);
-
-	TURN_OFF_BITS(TPM1_MOD,0xFFFFU, 0);
-	TURN_ON_BITS(TPM1_MOD, 49U, 0);
+	/*Counting 50 pulses*/
+	SET_BITS(TPM1_MOD, TPM_MOD, 16, 0);
 }
 
 /* ************************************************ */
@@ -43,10 +39,11 @@ void PWM_init(void){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void coolerfan_init(void){
-	TURN_OFF_BITS(COLLOERFAN_TPMx_CnSC,0b11U, 4);
-	TURN_ON_BITS(COLLOERFAN_TPMx_CnSC, 0b10u, 4);
+	SET_BITS(COLLOERFAN_TPMx_CnSC, TPM_MSnx, 2, 4);
+	SET_BITS(COLLOERFAN_TPMx_CnSC, TPM_ELSnx, 2, 2);
 
-	TURN_ON_BITS(COLLOERFAN_PORTx_PCRn, 0b11U, 8);
+	SET_BITS(COLLOERFAN_PORTx_PCRn, CONFIG_PORT_AS_PWM, 3, 8);
+	coolerfan_PWMDuty(0);
 }
 
 /* ************************************************ */
@@ -56,10 +53,11 @@ void coolerfan_init(void){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void heater_init(void){
-	TURN_OFF_BITS(COLLOERFAN_TPMx_CnSC,0xFFFFFFFFU, 4);
-	TURN_ON_BITS(COLLOERFAN_TPMx_CnSC, 0b10u, 4);
+	SET_BITS(HEADER_TPMx_CnSC, TPM_MSnx, 2, 4);
+	SET_BITS(HEADER_TPMx_CnSC, TPM_ELSnx, 2, 2);
 
-	TURN_ON_BITS(	HEADER_PORTx_PCRn, 0b11U, 8);
+	SET_BITS(	HEADER_PORTx_PCRn, CONFIG_PORT_AS_PWM, 3, 8);
+	heater_PWMDuty(0);
 }
 
 /* ************************************************ */
@@ -71,8 +69,7 @@ void heater_init(void){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void coolerfan_PWMDuty(float fCoolerDuty){
-	TURN_OFF_BITS(COLLOERFAN_TPMx_CnV, 0xFFFFFFFFU, 0);
-	TURN_ON_BITS(COLLOERFAN_TPMx_CnV, (unsigned int)fCoolerDuty*50, 0);
+	SET_BITS(COLLOERFAN_TPMx_CnV, (unsigned int)fCoolerDuty*50, 16, 0);
 }
 
 /* ************************************************ */
@@ -84,6 +81,5 @@ void coolerfan_PWMDuty(float fCoolerDuty){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void heater_PWMDuty(float fHeaterDuty){
-	TURN_OFF_BITS(HEADER_TPMx_CnV, 0xFFFFFFFFU, 0);
-	TURN_ON_BITS(HEADER_TPMx_CnV, (unsigned int)fHeaterDuty*50, 0);
+	SET_BITS(HEADER_TPMx_CnV, (unsigned int)fHeaterDuty*50, 16, 0);
 }
--- Aula 11-12/Sources/board.h
+++ Aula 11-12/Sources/board.h
@@ -11,25 +11,30 @@
 #define SOURCES_BOARD_H_
 
 /*                 General uC definitions                 */
+
+#define SET_BITS(OUTPUT, INPUT, NUM_BITS, TO_SHIFT)               (OUTPUT = ((OUTPUT & ( ~( (0XFFFFFFFFU  >> (32 - NUM_BITS)) << TO_SHIFT) ) ) | (INPUT << TO_SHIFT)) )
+
 /* Clock gate control */
-#define  CGC_CLOCK_DISABLED         0x00U
-#define  CGC_CLOCK_ENABLED          0x01U
+#define  CGC_CLOCK_DISABLED     0x00U
+#define  CGC_CLOCK_ENABLED		0x01U
+#define PORT_A_TO_SHIFT			0x09U
 
-#define TURN_ON_BITS(OUTPUT, INPUT, TO_SHIFT)               (OUTPUT |= INPUT << TO_SHIFT)
-#define TURN_OFF_BITS(OUTPUT, INPUT,TO_SHIFT)               (OUTPUT &= 0XFFFFFFFFU  ^ ( INPUT<< TO_SHIFT))
+#define PRESCALER				0b110U		/*Prescale Factor Selection, 110 Divide by 64*/
+#define TPM_CLOCK				0b11U		/*TPM clock source select, 11 MCGIRCLK clock (32KHz)*/
+#define TPM_CMOD				0b01U		/*Clock Mode Selection *,01 LPTPM counter increments on every LPTPM counter clock */
+#define TPM_CPWMS				0b0U		/*Center-aligned PWM Select,0 LPTPM counter operates in up counting mode.*/
+#define TPM_MOD					49U			/*Counting 50 pulses*/
+#define TPM_MSnx				0b10U		/*Edge-aligned PWM*/
+#define TPM_ELSnx				0b10U		/*High-true pulses (clear Output on match, set Output on reload)*/
+#define CONFIG_PORT_AS_PWM		0b011U		/*011 Alternative 3 (chip-specific).*/
 
-#define COLLOERFAN_BASE_PNT                PORTA                                   /* peripheral port base pointer */
-#define COLLOERFAN_GPIO_BASE_PNT           PTA                                     /* peripheral gpio base pointer */
-#define COLLOERFAN_PORTx_PCRn			PORTA_PCR13
 
-#define HEADER_BASE_PNT                PORTA                                   /* peripheral port base pointer */
-#define HEADER_GPIO_BASE_PNT           PTA                                     /* peripheral gpio base pointer */
-#define HEADER_PORTx_PCRn			PORTA_PCR12
+#define COLLOERFAN_PORTx_PCRn	PORTA_PCR13
+#define COLLOERFAN_TPMx_CnSC 	TPM1_C0SC
+#define COLLOERFAN_TPMx_CnV 	TPM1_C0V
 
-#define COLLOERFAN_TPMx_CnSC TPM1_C0SC
-#define HEADER_TPMx_CnSC TPM1_C1SC
-
-#define COLLOERFAN_TPMx_CnV TPM1_C0V
-#define HEADER_TPMx_CnV TPM1_C1V
+#define HEADER_PORTx_PCRn		PORTA_PCR12
+#define HEADER_TPMx_CnSC 		TPM1_C1SC
+#define HEADER_TPMx_CnV 		TPM1_C1V
 
 #endif /* SOURCES_BOARD_H_ */
