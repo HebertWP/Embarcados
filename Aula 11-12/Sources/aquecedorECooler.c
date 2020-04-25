9561ed3c0a4177744877cd1dc343459084e3770b
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-04-25 08:51:51 -0300

    some commentary added

--- Aula 11-12/Sources/aquecedorECooler.c
+++ Aula 11-12/Sources/aquecedorECooler.c
@@ -1,35 +1,46 @@
+/* ***************************************************************** */
+/* File name:        aquecedorECooler.c                              */
+/* File description: File containing the implementation of functions */
+/*                   used to control the CoolerFan and Header        */
+/* Author name:      Caio Villela, Hebert Wandick                    */
+/* Creation date:    21/apr/2020                                     */
+/* Revision date:    24/apr/2020                                     */
+/* ***************************************************************** */
+
+/*my  includes*/
+#include "board.h"
+
+/*system include*/
+#include "aquecedorECooler.h"
+#include "fsl_clock_manager.h"
+
 /* ************************************************ */
 /* Method name:        PWM_init	                    */
 /* Method description: Initialize PWM				*/
 /* Input params:       n/a                          */
 /* Output params:      n/a                          */
 /* ************************************************ */
-
-#include "board.h"
-#include "aquecedorECooler.h"
-#include "fsl_clock_manager.h"
-
 void PWM_init(void){
-	/*un-gateport A clock*/
-	SET_BITS(SIM_SCGC5, CGC_CLOCK_ENABLED, 1, PORT_A_TO_SHIFT);
+    /*un-gateport A clock*/
+    SET_BITS(SIM_SCGC5, CGC_CLOCK_ENABLED, 1, PORT_A_TO_SHIFT);
 
-	/*un-gateport C clock*/
-	SET_BITS(SIM_SCGC6, CGC_CLOCK_ENABLED, 1, 25);
+    /*un-gateport TPM1 clock*/
+    SET_BITS(SIM_SCGC6, CGC_CLOCK_ENABLED, 1, 25);
 
-	/*Prescale Factor Selection, 110 Divide by 64*/
-	SET_BITS(TPM1_SC, PRESCALER, 3, 0);
+    /*Prescale Factor Select at TPM1_SC on bytes 2-0*/
+    SET_BITS(TPM1_SC, PRESCALER, 3, 0);
 
-	/*TPM clock source select, 11 MCGIRCLK clock (32KHz)*/
-	SET_BITS(SIM_SOPT2, TPM_CLOCK, 2, 24);
+    /*TPM clock source select at SIM_SOPT2 on bytes 24-25*/
+    SET_BITS(SIM_SOPT2, TPM_CLOCK, 2, 24);
 
-	/*Clock Mode Selection *,01 LPTPM counter increments on every LPTPM counter clock */
-	SET_BITS(TPM1_SC, TPM_CMOD, 2, 3);
+    /*Clock Mode Selection at TPM1_SC on bytes 3-4*/
+    SET_BITS(TPM1_SC, TPM_CMOD, 2, 3);
 
-	/*Center-aligned PWM Select,0 LPTPM counter operates in up counting mode.*/
-	SET_BITS(TPM1_SC, TPM_CPWMS, 1, 5);
+    /*Center-aligned PWM Select,0 LPTPM counter operates in up counting mode.*/
+    SET_BITS(TPM1_SC, TPM_CPWMS, 1, 5);
 
-	/*Counting 50 pulses*/
-	SET_BITS(TPM1_MOD, TPM_MOD, 16, 0);
+    /*Counting 50 pulses*/
+    SET_BITS(TPM1_MOD, TPM_MOD, 16, 0);
 }
 
 /* ************************************************ */
@@ -39,11 +50,13 @@ void PWM_init(void){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void coolerfan_init(void){
-	SET_BITS(COLLOERFAN_TPMx_CnSC, TPM_MSnx, 2, 4);
-	SET_BITS(COLLOERFAN_TPMx_CnSC, TPM_ELSnx, 2, 2);
+    /*Set MSnA:MSnB and ELSnA:ELSnB*/
+    SET_BITS(COOLERFAN_TPMx_CnSC, TPM_MSnx, 2, 4);
+    SET_BITS(COOLERFAN_TPMx_CnSC, TPM_ELSnx, 2, 2);
 
-	SET_BITS(COLLOERFAN_PORTx_PCRn, CONFIG_PORT_AS_PWM, 3, 8);
-	coolerfan_PWMDuty(0);
+    /*Config port as PWM port */
+    SET_BITS(COOLERFAN_PORTx_PCRn, CONFIG_PORT_AS_PWM, 3, 8);
+    coolerfan_PWMDuty(0);
 }
 
 /* ************************************************ */
@@ -53,11 +66,13 @@ void coolerfan_init(void){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void heater_init(void){
-	SET_BITS(HEADER_TPMx_CnSC, TPM_MSnx, 2, 4);
-	SET_BITS(HEADER_TPMx_CnSC, TPM_ELSnx, 2, 2);
+    /*Set MSnA:MSnB and ELSnA:ELSnB*/
+    SET_BITS(HEADER_TPMx_CnSC, TPM_MSnx, 2, 4);
+    SET_BITS(HEADER_TPMx_CnSC, TPM_ELSnx, 2, 2);
 
-	SET_BITS(	HEADER_PORTx_PCRn, CONFIG_PORT_AS_PWM, 3, 8);
-	heater_PWMDuty(0);
+    /*Config port as PWM port */
+    SET_BITS(HEADER_PORTx_PCRn, CONFIG_PORT_AS_PWM, 3, 8);
+    heater_PWMDuty(0);
 }
 
 /* ************************************************ */
@@ -69,7 +84,7 @@ void heater_init(void){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void coolerfan_PWMDuty(float fCoolerDuty){
-	SET_BITS(COLLOERFAN_TPMx_CnV, (unsigned int)fCoolerDuty*50, 16, 0);
+    SET_BITS(COOLERFAN_TPMx_CnV, (unsigned int)fCoolerDuty*50, 16, 0);
 }
 
 /* ************************************************ */
@@ -81,5 +96,5 @@ void coolerfan_PWMDuty(float fCoolerDuty){
 /* Output params:      n/a                          */
 /* ************************************************ */
 void heater_PWMDuty(float fHeaterDuty){
-	SET_BITS(HEADER_TPMx_CnV, (unsigned int)fHeaterDuty*50, 16, 0);
+    SET_BITS(HEADER_TPMx_CnV, (unsigned int)fHeaterDuty*50, 16, 0);
 }
--- Aula 11-12/Sources/board.h
+++ Aula 11-12/Sources/board.h
@@ -5,7 +5,7 @@
 /*                                                                   */
 /* Author name:      Hebert Wandick / Caio Villela                   */
 /* Creation date:    24bri2020                                      */
-/* Revision date:    24abri2020                                      */
+/* Revision date:    25abri2020                                      */
 /* ***************************************************************** */
 #ifndef SOURCES_BOARD_H_
 #define SOURCES_BOARD_H_
@@ -15,9 +15,9 @@
 #define SET_BITS(OUTPUT, INPUT, NUM_BITS, TO_SHIFT)               (OUTPUT = ((OUTPUT & ( ~( (0XFFFFFFFFU  >> (32 - NUM_BITS)) << TO_SHIFT) ) ) | (INPUT << TO_SHIFT)) )
 
 /* Clock gate control */
-#define  CGC_CLOCK_DISABLED     0x00U
-#define  CGC_CLOCK_ENABLED		0x01U
-#define PORT_A_TO_SHIFT			0x09U
+#define  CGC_CLOCK_DISABLED     0x00U       /*Disable clock*/
+#define  CGC_CLOCK_ENABLED		0x01U       /*Enable clock*/
+#define PORT_A_TO_SHIFT			0x09U       /*Select byte to be turn on*/
 
 #define PRESCALER				0b110U		/*Prescale Factor Selection, 110 Divide by 64*/
 #define TPM_CLOCK				0b11U		/*TPM clock source select, 11 MCGIRCLK clock (32KHz)*/
@@ -28,13 +28,12 @@
 #define TPM_ELSnx				0b10U		/*High-true pulses (clear Output on match, set Output on reload)*/
 #define CONFIG_PORT_AS_PWM		0b011U		/*011 Alternative 3 (chip-specific).*/
 
+#define COOLERFAN_PORTx_PCRn	PORTA_PCR13 /*Cooler connect at portA pin 13*/
+#define COOLERFAN_TPMx_CnSC 	TPM1_C0SC   /*Cooler connect at TPM1 on CH 0*/
+#define COOLERFAN_TPMx_CnV 	TPM1_C0V    /*Cooler connect at TPM1 on CH 0*/
 
-#define COLLOERFAN_PORTx_PCRn	PORTA_PCR13
-#define COLLOERFAN_TPMx_CnSC 	TPM1_C0SC
-#define COLLOERFAN_TPMx_CnV 	TPM1_C0V
-
-#define HEADER_PORTx_PCRn		PORTA_PCR12
-#define HEADER_TPMx_CnSC 		TPM1_C1SC
-#define HEADER_TPMx_CnV 		TPM1_C1V
+#define HEADER_PORTx_PCRn		PORTA_PCR12 /*Header connect at portA pin 12*/
+#define HEADER_TPMx_CnSC 		TPM1_C1SC   /*Header connect at TPM1 on CH 1*/
+#define HEADER_TPMx_CnV 		TPM1_C1V    /*Header connect at TPM1 on CH 1*/
 
 #endif /* SOURCES_BOARD_H_ */
