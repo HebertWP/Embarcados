13ae4a4aae69af7b78251b994955ea93138184b3
cvillela <csvillela1@gmail.com>
2020-05-20 18:50:07 -0300

    began main function

---
+++ Aula 15-16/Sources/main.c
@@ -0,0 +1,17 @@
+/* ***************************************************************** */
+/* File name:        main.c                                          */
+/* File description: implements UART configuration and interruption  */
+/*                                                                   */
+/* Author name:      Caio Villela/ Hebert Wandick                    */
+/* Creation date:    20/may/2020                                     */
+/* Revision date:    20/may/2020                                     */
+/* ***************************************************************** */
+
+#include "fsl_device_registers.h"
+#include "UART.h"
+#include "print_scan.h"
+#include "util.h"
+
+UART0_init()
+UART0_enableIRQ()
+UART0_IRQHandler()
