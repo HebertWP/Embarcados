c0db11e587a26684d83eb6604beffa1f459aa0be
cvillela <csvillela1@gmail.com>
2020-05-24 13:50:34 -0300

    updated util.c

--- Aula 15-16/Sources/util.c
+++ Aula 15-16/Sources/util.c
@@ -1,7 +1,44 @@
+typedef union
+{
+  unsigned char ucBytes[4];
+  float fReal;
+}floatUCharType;
 
-void setParam(unsigned char ucParam, unsigned char ucByte){
+float uChatToFloat(unsigned char ucValue){
+  floatUCharType varFloatUChar;
+  static unsigned char ucCount;
+
+  varFloatUChar.ucBytes[ucCount] = ucValue;
+
+  if(++ucCount >= 4){
+      ucCount = 0;
+      return(varFloatUChar.fReal);
+  }
+}
+
+unsigned char floatToUChar(int flag, float fReceived){
+  floatUCharType varFloatUChar;
+  unsigned char ucSendChar;
+  static unsigned char ucCount;
+
+  varFloatUChar.fReal = fReceived;
+
+  if(flag == 0){
+    ucCount = 0;
+    return(varFloatUChar.ucBytes[0]);
+  }
+  if(ucCount < 4){
+    ucCount++ ;
+    return(varFloatUChar.ucBytes[ucCount]);
+  }
+}
+
+
+
+void setParam(unsigned char ucParam, unsigned char ucValue){
+
+
   if('b' == ucParam){
-
     if('0' == ucByte){
       /*desliga botao*/
     } else {
@@ -14,6 +51,6 @@ void setParam(unsigned char ucParam, unsigned char ucByte){
   }
 }
 
-void answerParam(unsigned char ucParam, unsigned char ucByte){
-  
+void answerParam(unsigned char ucParam, unsigned char ucValue){
+  /*cria mensagem de answer*/
 }
