3ff521aca5aac96988d7f638574a5e8fa48198fb
cvillela <csvillela1@gmail.com>
2020-07-12 17:45:55 -0300

    removed extra files

--- Aula 01-02/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Freescale/KSDK_1.2.0/lib/ksdk_platform_lib/kds/KL25Z4/debug/libksdk_platform.a
-
-LIBS :=
-
