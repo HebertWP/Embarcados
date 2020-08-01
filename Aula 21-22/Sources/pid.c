adb891bf7384ecc323633dcff9f9afb9dcfa890d
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-08-01 14:39:50 -0300

    arrumando cabeçalho

--- Aula 21-22/Sources/pid.c
+++ Aula 21-22/Sources/pid.c
@@ -2,9 +2,10 @@
 /* File name:        pid.c                                           */
 /* File description: This file has a couple of useful functions to   */
 /*                   control the implemented PID controller          */
-/* Author name:      julioalvesMS, IagoAF, rBacurau                  */
+/* Author name:      julioalvesMS, IagoAF, rBacurau, Hebert Wandick  */
+/*					 Caio Villela					                 */
 /* Creation date:    21jun2018                                       */
-/* Revision date:    27mai2020                                       */
+/* Revision date:    01gos2020                                       */
 /* ***************************************************************** */
 
 #include "pid.h"
